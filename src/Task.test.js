c1ebbaf5106fe27e07d306a4b37ca9554714d8a6
che <che.blancoo58@gmail.com>
2021-03-25 10:24:53 +0100

    Add more tests for Task module

--- src/Task.test.js
+++ src/Task.test.js
@@ -5,3 +5,18 @@ test("creates instance of class",()=>{
   const newTask = new Task("abc","abcg","2021-03-19","high","1616531323647","default")
   expect(newTask).toBeInstanceOf(Task)
 })
+test("constructor does not throw error when called without parameters",()=>{
+  const newTask = new Task()
+  expect(()=>newTask).not.toThrow()
+})
+test("constructor throws error when called with invalid parameters",()=>{
+  expect(()=>new Task(a,"ab","c","d","e","f")).toThrow()
+})
+test("creates instance of an array class",()=>{
+ expect(Task.getTask()).toBeInstanceOf(Array)
+
+})
+test("throws an error if the storeTask method is called withou argument",()=>{
+  expect(Task.storeTask()).toThrow()
+ 
+ })
